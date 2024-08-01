7057decadbb0659dc27104394921280dd8087871
brepp22 <brepp@hotmail.com>
2024-07-31 19:54:44 -0700

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,18 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
-
-  development: {
+const sharedConfig = {
     client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    migrations: {directory : './data/migrations'},
+    useNullAsDefault : true , 
+    pool : { afterCreate: (conn , done) => conn.run('PRAGMA foreign_keys = ON' , done)},
   }
 
-};
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection : { filename: './data/cook_book.db3'},
+    seeds: { directory: './data/seeds'},
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename : './data/cook_book.test.db3'},
+  },
+}
